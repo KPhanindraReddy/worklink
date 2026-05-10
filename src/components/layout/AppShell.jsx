65e758ee1060fd8af131d43375fd02722e57c0c7
KPhanindraReddy <phanindrakamana50101@gmail.com>
2026-05-10 08:48:26 +0530

    v2.4

--- src/components/layout/AppShell.jsx
+++ src/components/layout/AppShell.jsx
@@ -1,10 +1,12 @@
 import { Footer } from './Footer';
 import { BottomDockNav } from './BottomDockNav';
+import { GlobalFeatureLauncher } from './GlobalFeatureLauncher';
 import { Navbar } from './Navbar';
 
 export const AppShell = ({ children }) => (
   <div className="min-h-screen">
     <Navbar />
+    <GlobalFeatureLauncher />
     <div className="app-shell-content">
       <main>{children}</main>
       <Footer />
